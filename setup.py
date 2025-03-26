7041e1b555d560d713eedcce7163acc2b5165239
HemanthD13 <hemanth.dhamodha@tigeranalytics.com>
2025-03-26 14:35:30 +0530

    Fixed workflow

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],
+    install_requires=["numpy", "pandas", "scikit-learn"],
     entry_points={
-        "console_scripts": ["run-nonstandard = mle_training.nonstandardcode:main"]
+        "console_scripts": [
+            "run-nonstandard = mle_training.nonstandardcode:main",
+            "run-ingest = mle_training.data_ingestion:main",
+            "run-train = mle_training.train:main",
+            "run-score = mle_training.score:main",
+        ]
     },
+    python_requires=">=3.8",
 )
